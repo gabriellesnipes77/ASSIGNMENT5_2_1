1a94faba6b8afae3e7ea4f09062f23965adb017e
BIG DON <BIG DON@MSI>
2024-12-03 08:42:10 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿namespace ASSIGNMENT5_2_1
+{
+    internal class Program
+    {
+        public static int LengthOfLastWord(string s)
+
+        {
+            s = s.Trim();
+
+
+            int length = 0;
+
+
+            for (int i = s.Length - 1; i >= 0; i--)
+            {
+                if (s[i] == ' ' && length > 0)
+                {
+                    break;
+                }
+
+                if (s[i] != ' ')
+                {
+                    length++;
+                }
+            }
+
+            return length;
+        }
+
+         public static void Main(string[] args)
+        {
+            Console.WriteLine(LengthOfLastWord("Hello, World!"));
+            Console.WriteLine(LengthOfLastWord("fly me to the moon"));
+            Console.WriteLine(LengthOfLastWord("word"));
+            Console.WriteLine(LengthOfLastWord("singleWord"));
+        }
+    }
+}
